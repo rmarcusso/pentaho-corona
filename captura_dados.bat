762f337afc62d11199c36d4bc4af27b1a2e1f8ff
Romildo Marcusso <romildo.marcusso@gmail.com>
2020-08-16 12:22:13 -0300

    atualizacao dumps

--- captura_dados.bat
+++ captura_dados.bat
@@ -1,6 +1,6 @@
 set nome_arquivo=dump_corona_%DATE:~3,2%%DATE:~0,2%%DATE:~6,4%
-c:\PostgreSQL\pg95\bin\pg_dump -U postgres -d corona > c:\Users\romildo.albuquerque\Documents\Corona\git\pentaho-corona\Dump\%nome_arquivo%.sql
-cd C:\Users\romildo.albuquerque\Documents\Corona\git\pentaho-corona\Dump\
+c:\PostgreSQL\pg95\bin\pg_dump -U postgres -d corona > c:\Users\romildo.albuquerque\Documents\git\pentaho-corona\Dump\%nome_arquivo%.sql
+cd C:\Users\romildo.albuquerque\Documents\git\pentaho-corona\Dump\
 jar -cMf %nome_arquivo%.zip %nome_arquivo%.sql
-del c:\Users\romildo.albuquerque\Documents\Corona\git\pentaho-corona\Dump\%nome_arquivo%.sql
+del c:\Users\romildo.albuquerque\Documents\git\pentaho-corona\Dump\%nome_arquivo%.sql
 C:\pentaho\pdi83\Kitchen.bat /file C:\pentaho\pentaho-server-ce-8.3.0.0-371\pentaho-solutions\system\CoronaVirus\endpoints\kettle\coletadados.kjb
