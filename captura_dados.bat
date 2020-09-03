09b228a240a8cffed4231992f3be2d3b63c5bb6c
Romildo Marcusso <romildo.marcusso@gmail.com>
2020-09-02 21:47:23 -0300

    Atualizacao captura.bat

--- captura_dados.bat
+++ captura_dados.bat
@@ -1,6 +1,7 @@
-set nome_arquivo=dump_corona_%DATE:~3,2%%DATE:~0,2%%DATE:~6,4%
-c:\PostgreSQL\pg95\bin\pg_dump -U postgres -d corona > c:\Users\romildo.albuquerque\Documents\git\pentaho-corona\Dump\%nome_arquivo%.sql
-cd C:\Users\romildo.albuquerque\Documents\git\pentaho-corona\Dump\
-jar -cMf %nome_arquivo%.zip %nome_arquivo%.sql
-del c:\Users\romildo.albuquerque\Documents\git\pentaho-corona\Dump\%nome_arquivo%.sql
+echo GERANDO DUMP
+c:\PostgreSQL\pg95\bin\pg_dump -U postgres -d corona > c:\Users\romildo.albuquerque\Documents\git\pentaho-corona\Dump\dump_corona.sql
+echo DUMP GERADO E EXECUTANDO NOVA CAPTURA
+jar -cfM c:\Users\romildo.albuquerque\Documents\git\pentaho-corona\Dump\dump_corona.zip c:\Users\romildo.albuquerque\Documents\git\pentaho-corona\Dump\dump_corona.sql
+del c:\Users\romildo.albuquerque\Documents\git\pentaho-corona\Dump\dump_corona.sql
 C:\pentaho\pdi83\Kitchen.bat /file C:\pentaho\pentaho-server-ce-8.3.0.0-371\pentaho-solutions\system\CoronaVirus\endpoints\kettle\coletadados.kjb
+pause
